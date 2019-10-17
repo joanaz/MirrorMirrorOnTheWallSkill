700844ff7fb3ce85e402cd3b03d722ab7873b66a
Raymond <lir@Win7-Desktop>
2019-10-16 23:15:09 -0700

    - merged pull request #43 and #32 - added google translate - fix async issues in handlers

--- src/MirrorMirror.js
+++ src/MirrorMirror.js
@@ -1,3 +1,4 @@
+const Keys = require("./certs/keys.json");
 var awsIot = require('aws-iot-device-sdk');
 
 var app = {}
@@ -6,76 +7,99 @@ app.TOPIC_IMAGES = "MagicMirror:new-images"
 app.TOPIC_TEXT = "MagicMirror:new-text"
 app.TOPIC_MODULE = "MagicMirror:change-module"
 app.TOPIC_VIDEO = "MagicMirror:new-video"
+app.TOPIC_PICTURE = "MagicMirror:try-on"
+
 
 app.setup = function() {
-  app.device = awsIot.device({
-    keyPath: __dirname + "/certs/MagicMirror.private.key",
-    certPath: __dirname + "/certs/MagicMirror.cert.pem",
-    caPath: __dirname + "/certs/root-CA.crt",
-    clientId: "MirrorMirror" + (new Date().getTime()),
-    region: "us-east-1",
-    host: "YOURID.iot.us-east-1.amazonaws.com",
-  });
+	if (app.device) {app.device.end();}
 
-  app.device.on('connect', function() {
-    console.log('connect');
-  });
-
-  app.device.on('message', function(topic, payload) {
-    console.log('message', topic, payload.toString());
-  });
+	app.device = awsIot.device({
+	    keyPath: __dirname + "/certs/MagicMirror.private.key",
+	    certPath: __dirname + "/certs/MagicMirror.cert.pem",
+	    caPath: __dirname + "/certs/root-CA.crt",
+	    host: Keys.thing.host
+	});
 }
 
 // Method that will accept a text and publish to AWS IoT
-app.displayText = function(text, callback) {
-  var update = {
-    "displayText": text
-  };
+app.displayText = function(text) {
+	return new Promise(((resolve, reject) => {
+	  app.setup();
+	  var update = {
+	    "displayText": text
+	  };
 
-  app.device.publish(app.TOPIC_TEXT, JSON.stringify(update), function() {
-    console.log("Published: \nTopic => " + app.TOPIC_TEXT + "Data => " + JSON.stringify(update));
-    callback()
-  });
+	  app.device.publish(app.TOPIC_TEXT, JSON.stringify(update), function() {
+	    app.device.end();
+	    resolve({});
+	  });
+	}));
 }
 
 // Method that will accept an array of images and publish to AWS IoT
-app.showImages = function(images, searchTerm, callback) {
-  var update = {
-    "images": images,
-    "displayText": searchTerm,
-  };
+app.showImages = function(images, searchTerm) {
+	return new Promise(((resolve, reject) => {
+		app.setup();
+  		var update = {
+  		  "images": images,
+  		  "displayText": searchTerm,
+  		};
 
-  app.device.publish(app.TOPIC_IMAGES, JSON.stringify(update), function() {
-    console.log("Published: \nTopic => " + app.TOPIC_IMAGES + "Data => " + JSON.stringify(update));
-    callback()
-  });
+  		app.device.publish(app.TOPIC_IMAGES, JSON.stringify(update), function() {
+  		    app.device.end();
+  		  	resolve({});
+  		});
+	}));
 }
 
 // Method that will accept a Magic Mirror Module name and publish to AWS IoT
-app.changeModule = function(moduleName, turnOn, callback) {
-  var update = {
-    "moduleName": moduleName,
-    "turnOn": turnOn,
-  };
+app.changeModule = function(moduleName, turnOn) {
+	return new Promise(((resolve, reject) => {
+		app.setup();
+		var update = {
+		    "moduleName": moduleName,
+		    "turnOn": turnOn,
+		};
 
-  app.device.publish(app.TOPIC_MODULE, JSON.stringify(update), function() {
-    console.log("Published: \nTopic => " + app.TOPIC_MODULE + "Data => " + JSON.stringify(update));
-    callback()
-  });
+		app.device.publish(app.TOPIC_MODULE, JSON.stringify(update), function() {
+		    console.log("Published: \nTopic => " + app.TOPIC_MODULE + "Data => " + JSON.stringify(update));
+		    app.device.end();
+		    resolve({});
+		});
+	}));
+}
+
+app.takePicture = function() {
+	return new Promise(((resolve, reject) => {
+		app.setup();
+		var update = {
+  		  	"takePicture": true,
+  		};
+
+  		app.device.publish(app.TOPIC_PICTURE, JSON.stringify(update), function() {
+  		  console.log("Published: \nTopic => " + app.TOPIC_PICTURE + "Data => " + JSON.stringify(update));
+  		  app.device.end();
+  		  resolve({});
+  		});
+	}));
 }
 
 
 // Method that will accept a video ID and publish to AWS IoT
 app.showVideo = function(videoId, searchTerm, callback) {
-  var update = {
-    "videoId": videoId,
-    "displayText": searchTerm,
-  };
+	return new Promise(((resolve, reject) => {
+		app.setup();
+		var update = {
+			"videoId": videoId,
+		    "displayText": searchTerm,
+		};
 
-  app.device.publish(app.TOPIC_VIDEO, JSON.stringify(update), function() {
-    console.log("Published: \nTopic => " + app.TOPIC_VIDEO + "Data => " + JSON.stringify(update));
-    callback()
-  });
+  		app.device.publish(app.TOPIC_VIDEO, JSON.stringify(update), function() {
+  		  console.log("Published: \nTopic => " + app.TOPIC_VIDEO + "Data => " + JSON.stringify(update));
+  		  app.device.end();
+  		  resolve({});
+  		});
+	}));
 }
 
 module.exports = app
